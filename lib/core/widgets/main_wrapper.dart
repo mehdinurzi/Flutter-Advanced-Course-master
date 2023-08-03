3b8689caeff611e8e56cb025eab506b1271d6465
mehdinurzi <mehdinoroozi13788@gmail.com>
2023-08-03 18:53:01 +0330

    first commit

---
+++ lib/core/widgets/main_wrapper.dart
@@ -0,0 +1,44 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_advanced_course/core/widgets/app_background.dart';
+import 'package:flutter_advanced_course/features/feature_bookmark/presentation/screens/bookmark_screen.dart';
+import 'package:flutter_advanced_course/features/feature_weather/presentation/screens/home_screen.dart';
+
+import 'bottom_nav.dart';
+
+
+class MainWrapper extends StatelessWidget {
+  MainWrapper({Key? key}) : super(key: key);
+
+  final PageController pageController = PageController(initialPage: 0);
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    List<Widget> pageViewWidget = [
+      const HomeScreen(),
+      BookMarkScreen(pageController: pageController,),
+    ];
+
+    var height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      extendBody: true,
+      bottomNavigationBar: BottomNav(Controller: pageController),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AppBackground.getBackGroundImage(),
+              fit: BoxFit.cover
+          ),
+        ),
+        height: height,
+        child: PageView(
+          controller: pageController,
+          children: pageViewWidget,
+        ),
+      ),
+    );
+  }
+}
